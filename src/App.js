4e0c5ce81afa9898da251b97aa4ccf8ce850d917
Marcus Reaiche <marcusreaiche@gmail.com>
2019-12-07 21:30:35 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function App() {
+  
+  const [techs, setTechs] = useState([
+    "ReactJS",
+    "React Native",
+  ]);
+
+  const [input, setInput] = useState("");
+
+  function handleAdd() {
+    if(input && !techs.find(tech => tech === input)) {
+      setTechs([...techs, input])
+    }
+    setInput("");
+  }
+
   return (
-    <h1>
-      Hello, React!
-    </h1>
+    <>
+      <h1>My awesome techs</h1>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <input 
+        type="text" 
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      />
+      <button 
+        type="button"
+        onClick={handleAdd}
+      >
+        Add Tech
+      </button>
+    </>
   );
 }
 
